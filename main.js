97e9b85e6d1401d4b7744e7671ed1cb319e57da8
Laurian Mathiot <132049556+LaurianMathiot@users.noreply.github.com>
2023-06-27 11:35:53 +0200

    ajout des catégories de recettes

--- main.js
+++ main.js
@@ -1,31 +1,5 @@
-const button = document.querySelector("button")
-
-button.addEventListener("click", async () => {
-    const divRoot = document.querySelector("#root")
-
-    // Récuperer les données de l'API
-    // fetch sur l'url 
-    const responseJson = await fetch("https://www.themealdb.com/api/json/v1/1/search.php?s")
-    const responseJavascript = await responseJson.json()
-
-    // quand j'ai récupéré les données (recettes)
-    responseJavascript.meals.forEach(meal => {
-        // j'affiche le titre de chacune des recettes
-        const mealTitleElement = createNodeElement("h2",
-        {class:"meal-title"},
-        meal.strMeal
-        )
-
-        divRoot.appendChild(mealTitleElement)
-
-        const mealImgElement = createNodeElement("img", {
-            src: meal.strMealThumb,
-          });
-      
-          // j'insère l'image dans ma div root
-          divRoot.appendChild(mealImgElement);
-        });
-    })
+const divRoot = document.querySelector("#root")
+const divRoot2 = document.querySelector("#root2")
 
 const createNodeElement = (tagType, attributes, elementText = "") => {
     const nodeElement = document.createElement(tagType)
@@ -35,6 +9,88 @@ const createNodeElement = (tagType, attributes, elementText = "") => {
         nodeElement.setAttribute(property, attributes[property])
     }
 
-    // je retourne le résultat de ma fonction pour pouvoir l'utiliser en dehors de celle-ci
     return nodeElement
 }
+
+const contactFormComponent = () => {
+
+    const formElement = createNodeElement('form')
+    
+    const inputText = createNodeElement('input', {
+        type: "text",
+        class: "contact-text",
+    })
+    formElement.appendChild(inputText)
+
+    const submitBtnElement = createNodeElement ('button', {
+        type: "submit",
+        class: "contact-submit",
+        },
+        "Envoyer"
+    )
+    formElement.appendChild(submitBtnElement)
+
+    divRoot.appendChild(formElement)
+}
+
+contactFormComponent()
+
+const mealsList = async () => {    
+    
+    const responseJson = await fetch("https://www.themealdb.com/api/json/v1/1/search.php?s")
+    const responseJavascript = await responseJson.json()
+
+    const recepiesTitle = createNodeElement("h2",{class: "recepies-title"}, "Recettes")
+
+        divRoot.appendChild(recepiesTitle)
+    
+    
+    responseJavascript.meals.forEach(meal => {
+    
+        const mealTitleElement = createNodeElement("h2",
+        {class: "meal-title"},
+        meal.strMeal
+        )
+    
+        divRoot.appendChild(mealTitleElement)
+    
+        const mealImgElement = createNodeElement("img", {
+            src: meal.strMealThumb,
+            });
+        
+            divRoot.appendChild(mealImgElement);
+        });
+}
+
+mealsList()
+
+// je créé la fonction qui concerne les catégories
+const mealsCategories = async () => {
+    
+    // j'appelle l'API et je la traduis du Json en JS
+    const responseJson = await fetch("https://www.themealdb.com/api/json/v1/1/categories.php") 
+    const responseJavascript = await responseJson.json()
+
+        // je créé un titre "Catégories" à cette section
+        const categoriesTitle = createNodeElement("h2",{class: "categories-title"}, "Catégories")
+
+        // je l'intègre dans ma div
+        divRoot2.appendChild(categoriesTitle)
+
+    // je parcours les éléments de l'API 
+    responseJavascript.categories.forEach(meal => {
+
+        // je créé les éléments portant les noms des catégories et je leur applique une classe
+        const mealCategory = createNodeElement("h2",
+        {class: "meal-category"},
+        meal.strCategory
+        )
+
+        // je les envoie dans ma div
+        divRoot2.appendChild(mealCategory)
+
+    })
+}
+
+// je lance la fonction
+mealsCategories()
