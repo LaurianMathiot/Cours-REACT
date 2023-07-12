cb2e4cc8e5536afaeaa37cb4f111bc91be9283a7
Laurian Mathiot <132049556+LaurianMathiot@users.noreply.github.com>
2023-07-12 16:13:40 +0200

    spinner and settimeout / coworking-crud-front

--- coworking-crud-front/src/ListCoworkings.jsx
+++ coworking-crud-front/src/ListCoworkings.jsx
@@ -2,19 +2,21 @@ import { useState } from "react";
 import ShowCoworking from "./ShowCoworking";
 
 const ListCoworkings = () => {
-
-      
+     
       const [coworkings, setCoworkings] = useState([])
 
-      const FetchCoworkings = async () => {
+      const fetchCoworkings = async () => {
         if (coworkings.length === 0) {
+
+          setTimeout(async () => {
           const coworkingsResponse = await fetch("/coworkings.json");
-          setCoworkings(await coworkingsResponse.json()); 
-        }
+          setCoworkings(await coworkingsResponse.json());
+
+        }, 1500);
       }
-
-      FetchCoworkings()
-
+    }
+    
+    fetchCoworkings();
 
       const [filter, setFilter] = useState(null);
 
@@ -45,13 +47,17 @@ const ListCoworkings = () => {
         
 
         <div className="articles-container">
-        {filteredCoworkings.map((coworking) => {
-          return (
-            <>
-              <ShowCoworking coworking={coworking}/>
-            </>
-          );
-        })}
+        {filteredCoworkings.length === 0 ? (
+          <svg class="spinner" viewBox="0 0 50 50">
+            <circle class="path" cx="25" cy="25" r="20" fill="none" stroke-width="5"></circle>
+          </svg>
+        ) : (
+        <>
+          {filteredCoworkings.map((coworking) => {
+            return <ShowCoworking coworking={coworking} />;
+          })}
+        </>
+      )}
         </div>
       </section>
     );
